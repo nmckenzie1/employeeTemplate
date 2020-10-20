342a96fd7bed8c0ef08428c14ac67024db3f9c4f
Neil M <n.mckenzie@live.com>
2020-10-20 18:42:24 -0500

    added inquirer questions

--- app.js
+++ app.js
@@ -10,6 +10,139 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
+const ids = [];
+
+function teamBuilder() {
+    function createManager() {
+       inquirer.prompt([
+          {
+            type: "input",
+            name: "managerName",
+            message: "Welcome! Enter manager's name?",
+          },
+          {
+            type: "input",
+            name: "managerId",
+            message: "Enter manager's id?",
+          },
+          {
+            type: "input",
+            name: "managerEmail",
+            message: "Enter manager's email?",
+            
+          },
+          {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "Enter manager's office number?",
+          }
+        ]).then(responses => {
+          const manager = new Manager(responses.managerName, responses.managerId, responses.managerEmail, responses.managerOfficeNumber);
+          team.push(manager);
+          ids.push(responses.managerId);
+          createTeam();
+        });
+      }
+    
+      function createTeam() {
+    
+        inquirer.prompt([
+          {
+            type: "list",
+            name: "addEmployee",
+            message: "Would you like to add a team-mate??",
+            choices: [
+              "Engineer",
+              "Intern",
+              "Please god no more"
+            ]
+          }
+        ]).then(nextEmployee => {
+          switch(nextEmployee.addEmployee) {
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+          default:
+            buildTeam();
+          }
+        });
+      }
+    
+      function addEngineer() {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "engineerName",
+            message: "Enter engineer's name?",
+         },
+          {
+            type: "input",
+            name: "engineerId",
+            message: "Enter engineer's id?",
+          },
+          {
+            type: "input",
+            name: "engineerEmail",
+            message: "Enter engineer's email?",
+          },
+          {
+            type: "input",
+            name: "engineerGithub",
+            message: "Enter engineer's GitHub username?",
+          }
+        ]).then(responses => {
+          const engineer = new Engineer(responses.engineerName, responses.engineerId, responses.engineerEmail, responses.engineerGithub);
+          team.push(engineer);
+          ids.push(responses.engineerId);
+          createTeam();
+        });
+      }
+    
+      function addIntern() {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "internName",
+            message: "Enter intern's name?",
+           },
+          {
+            type: "input",
+            name: "internId",
+            message: "Enter intern's id?",
+            },
+          {
+            type: "input",
+            name: "internEmail",
+            message: "Enter intern's email?",
+          },
+          {
+            type: "input",
+            name: "internSchool",
+            message: "Enter intern's school?",
+          }
+        ]).then(responses => {
+          const intern = new Intern(responses.internName, responses.internId, responses.internEmail, responses.internSchool);
+          team.push(intern);
+          ids.push(responses.internId);
+          createTeam();
+        });
+      }
+    
+      function buildTeam() {
+       
+        if (!fs.existsSync(OUTPUT_DIR)) {
+          fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(team), "utf-8");
+      }
+    
+      createManager();
+}
+teamBuilder()
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
